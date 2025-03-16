5af4544eb362c7df459add185e1ac3b6a589b756
Cristian Adam <cristian.adam@qt.io>
2025-03-16 16:15:42 +0100

    Removed macOS and GTK specific code
    This specific code will be no longer needed in an upcoming commit that
    (ab)uses the QPlatformTheme specifics.
    
    Also moved the whole code into plugin.cpp

--- plugin.cpp
+++ plugin.cpp
@@ -1,165 +1,92 @@
-#include "plugin.h"
-
-#ifdef __APPLE__
-#include <objc/message.h>
-#include <objc/runtime.h>
-#endif
-
 #include <QDir>
 #include <QFile>
 #include <QIcon>
 #include <QSettings>
 
-#if defined(GTK3_FOUND) && !defined(__APPLE__) && !defined(_WIN32)
-#ifdef signals
-#undef signals
-#endif
-#include <gtk/gtk.h>
-#include <qobjectdefs.h>
-#endif
-
 #include <optional>
 
-#ifdef __APPLE__
-static IMP originalIconForFile;
-static QByteArray *folderIconData;
+#include <extensionsystem/iplugin.h>
 
-static id customThemeIconForFile(id objc_self, SEL objc_cmd, id fullPath) {
-    BOOL isDir = NO;
-    Class NSFileManager = objc_getClass("NSFileManager");
+#include <QStringList>
 
-    id defaultManager = ((id(*)(Class, SEL))objc_msgSend)(
-        NSFileManager, sel_registerName("defaultManager"));
+class IconThemePlugin final : public ExtensionSystem::IPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QtCreatorPlugin" FILE "nomoicontheme.json")
 
-    ((void (*)(id, SEL, id, id))objc_msgSend)(
-        defaultManager,
-        sel_registerName("fileExistsAtPath:isDirectory:"),
-        fullPath,
-        reinterpret_cast<id>(&isDir));
-
-    if (isDir) {
-        Class NSImage = objc_getClass("NSImage");
-        id imageMemory = ((id(*)(id, SEL))objc_msgSend)(
-            reinterpret_cast<id>(NSImage), sel_registerName("alloc"));
-        return ((id(*)(id, SEL, NSData *))objc_msgSend)(
-            imageMemory,
-            sel_registerName("initWithData:"),
-            folderIconData->toRawNSData());
-    }
-    return ((id(*)(id, SEL, id))originalIconForFile)(
-        objc_self, objc_cmd, fullPath);
-}
-#endif
-
-inline void init_resource() {
-    Q_INIT_RESOURCE(icontheme);
-}
-
-namespace IconTheme::Internal {
-
-static inline QByteArray getDesktopEnvironment() {
-    const auto xdgCurrentDesktop = qgetenv("XDG_CURRENT_DESKTOP");
-    if (!xdgCurrentDesktop.isEmpty()) {
-        return xdgCurrentDesktop.toUpper();
-    }
-    if (!qEnvironmentVariableIsEmpty("KDE_FULL_SESSION")) {
-        return QByteArrayLiteral("KDE");
-    }
-    if (!qEnvironmentVariableIsEmpty("GNOME_DESKTOP_SESSION_ID")) {
-        return QByteArrayLiteral("GNOME");
-    }
-    auto desktopSession = qgetenv("DESKTOP_SESSION");
-    int slash = desktopSession.lastIndexOf('/');
-    if (slash != -1) {
-        QSettings desktopFile(QFile::decodeName(desktopSession + ".desktop"),
-                              QSettings::IniFormat);
-        desktopFile.beginGroup(QStringLiteral("Desktop Entry"));
-        auto desktopName =
-            desktopFile.value(QStringLiteral("DesktopNames")).toByteArray();
-        if (!desktopName.isEmpty()) {
-            return desktopName;
+public:
+    static QByteArray getDesktopEnvironment()
+    {
+        const auto xdgCurrentDesktop = qgetenv("XDG_CURRENT_DESKTOP");
+        if (!xdgCurrentDesktop.isEmpty()) {
+            return xdgCurrentDesktop.toUpper();
         }
-        desktopSession = desktopSession.mid(slash + 1);
-    }
-    if (desktopSession == "gnome") {
-        return QByteArrayLiteral("GNOME");
-    }
-    if (desktopSession == "xfce") {
-        return QByteArrayLiteral("XFCE");
-    }
-    if (desktopSession == "kde") {
-        return QByteArrayLiteral("KDE");
-    }
-    return QByteArrayLiteral("UNKNOWN");
-}
-
-IconThemePlugin::IconThemePlugin() noexcept {
-    init_resource();
-
-    const auto fallbackThemeName = []() -> std::optional<QString> {
-        if (getDesktopEnvironment() == QByteArrayLiteral("KDE")) {
-            auto configDir = qEnvironmentVariable("XDG_CONFIG_DIRS");
-            if (configDir.isEmpty()) {
-                configDir = QDir::home().absolutePath() + "/.config";
-            }
-
-            auto settings =
-                QSettings(configDir + "/kdeglobals", QSettings::IniFormat);
-            auto val = settings.value("Icons/Theme");
-
-            if (val.canConvert<QString>()) {
-                return val.toString();
-            }
-
-            return std::nullopt;
+        if (!qEnvironmentVariableIsEmpty("KDE_FULL_SESSION")) {
+            return QByteArrayLiteral("KDE");
         }
-#if defined(GTK3_FOUND) && !defined(__APPLE__) && !defined(_WIN32)
-        else {
-            if (!gtk_init_check(nullptr, nullptr)) {
-                return QIcon::themeName();
-            }
-            GtkSettings *settings = gtk_settings_get_default();
-            gchararray iconThemeName;
-            g_object_get(
-                settings, "gtk-icon-theme-name", &iconThemeName, nullptr);
-            auto qIconThemeName = QString::fromUtf8(iconThemeName);
-            if (!qIconThemeName.isEmpty()) {
-                return qIconThemeName;
-            }
+        if (!qEnvironmentVariableIsEmpty("GNOME_DESKTOP_SESSION_ID")) {
+            return QByteArrayLiteral("GNOME");
         }
-#endif
-        return QIcon::themeName();
-    }();
-
-    QIcon::setThemeName("qtcreator-nomo");
-    if (fallbackThemeName.has_value()) {
-        QIcon::setFallbackThemeName(*fallbackThemeName);
+        auto desktopSession = qgetenv("DESKTOP_SESSION");
+        int slash = desktopSession.lastIndexOf('/');
+        if (slash != -1) {
+            QSettings desktopFile(QFile::decodeName(desktopSession + ".desktop"),
+                                  QSettings::IniFormat);
+            desktopFile.beginGroup(QStringLiteral("Desktop Entry"));
+            auto desktopName = desktopFile.value(QStringLiteral("DesktopNames")).toByteArray();
+            if (!desktopName.isEmpty()) {
+                return desktopName;
+            }
+            desktopSession = desktopSession.mid(slash + 1);
+        }
+        if (desktopSession == "gnome") {
+            return QByteArrayLiteral("GNOME");
+        }
+        if (desktopSession == "xfce") {
+            return QByteArrayLiteral("XFCE");
+        }
+        if (desktopSession == "kde") {
+            return QByteArrayLiteral("KDE");
+        }
+        return QByteArrayLiteral("UNKNOWN");
     }
 
-#ifdef __APPLE__
-    Class NSWorkspace = objc_getClass("NSWorkspace");
-    Method methodIconForFile =
-        class_getInstanceMethod(NSWorkspace, sel_registerName("iconForFile:"));
-    originalIconForFile = method_getImplementation(methodIconForFile);
-    method_setImplementation(methodIconForFile,
-                             reinterpret_cast<IMP>(customThemeIconForFile));
-    auto folderIconFile =
-        QFile(":/icons/qtcreator-nomo/scalable/places/folder.pdf");
-    folderIconFile.open(QIODevice::ReadOnly);
-    folderIconData = new QByteArray(folderIconFile.readAll());
-#endif
-}
+    explicit IconThemePlugin() noexcept
+    {
+        Q_INIT_RESOURCE(icontheme);
 
-bool IconThemePlugin::initialize([[maybe_unused]] const QStringList &arguments,
-                                 [[maybe_unused]] QString *errorString) {
-    return true;
-}
+        const auto fallbackThemeName = []() -> std::optional<QString> {
+            if (getDesktopEnvironment() == QByteArrayLiteral("KDE")) {
+                auto configDir = qEnvironmentVariable("XDG_CONFIG_DIRS");
+                if (configDir.isEmpty()) {
+                    configDir = QDir::home().absolutePath() + "/.config";
+                }
 
-ExtensionSystem::IPlugin::ShutdownFlag IconThemePlugin::aboutToShutdown() {
-#ifdef __APPLE__
-    delete folderIconData;
-#endif
-    return SynchronousShutdown;
-}
+                auto settings = QSettings(configDir + "/kdeglobals", QSettings::IniFormat);
+                auto val = settings.value("Icons/Theme");
 
-} // namespace IconTheme::Internal
+                if (val.canConvert<QString>()) {
+                    return val.toString();
+                }
+
+                return std::nullopt;
+            }
+
+            return QIcon::themeName();
+        }();
+
+        QIcon::setThemeName("qtcreator-nomo");
+        if (fallbackThemeName.has_value()) {
+            QIcon::setFallbackThemeName(*fallbackThemeName);
+        }
+    }
+
+    bool initialize(const QStringList &arguments, QString *errorString) override { return true; }
+
+    ExtensionSystem::IPlugin::ShutdownFlag aboutToShutdown() override
+    {
+        return SynchronousShutdown;
+    }
+};
+
+#include <plugin.moc>
